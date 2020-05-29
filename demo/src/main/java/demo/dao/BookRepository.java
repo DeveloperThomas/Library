f4c829785b1975c34e614adef8a645a498409f31
@pawelppp <pawel_puc@interia.pl>
2020-05-29 02:23:45 +0200

    filter books

--- demo/src/main/java/demo/dao/BookRepository.java
+++ demo/src/main/java/demo/dao/BookRepository.java
@@ -1,9 +1,10 @@
 package demo.dao;
 
 import demo.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
